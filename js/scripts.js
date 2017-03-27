1b5cd56179d0e75c722022f391dd5619ff9703d2
Brian Fan and Bryan Lin <brian@>
2017-03-27 13:35:28 -0700

    add deck of cards using 2 forEach loops, and also add second solution using filter method

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  //Word Play
   $("form").submit(function(event){
     event.preventDefault();
 
@@ -7,11 +9,18 @@ $(document).ready(function(){
     var sentence = $("input#sentence").val();
     var sentenceArr = sentence.split(" ");
 
+// Solution 1 using looping and branching
     sentenceArr.forEach(function(word) {
       if (word.length >= 3) {
         newSentence.push(word);
       }
     });
+
+// Solution 2 using Filters
+    // function isLongEnough(string) {
+    //   return string.length >= 3;
+    // }
+    // var newSentence = sentenceArr.filter(isLongEnough);
 
     newSentence = newSentence.reverse();
 
@@ -21,6 +30,19 @@ $(document).ready(function(){
     });
 
     alert(reverseSentence);
-
   });
+
+
+  // Build a deck
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  var suits = ["club", "heart", "diamond", "spade"];
+
+  suits.forEach(function(suit){
+    numbers.forEach(function(number){
+      $("#cards").append("<li>" + number + " of " + suit + "</li>");
+    });
+  });
+
+
 });
