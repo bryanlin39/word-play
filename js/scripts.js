b955cf55df9d4d48f3c3de380ead658a76630198
Brian Fan and Bryan Lin <brian@>
2017-03-27 15:11:14 -0700

    add forEach looping to find duplicate words inside an entered string

--- js/scripts.js
+++ js/scripts.js
@@ -34,15 +34,39 @@ $(document).ready(function(){
 
 
   // Build a deck
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  // var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  //
+  // var suits = ["club", "heart", "diamond", "spade"];
+  //
+  // suits.forEach(function(suit){
+  //   numbers.forEach(function(number){
+  //     $("#cards").append("<li>" + number + " of " + suit + "</li>");
+  //   });
+  // });
 
-  var suits = ["club", "heart", "diamond", "spade"];
 
-  suits.forEach(function(suit){
-    numbers.forEach(function(number){
-      $("#cards").append("<li>" + number + " of " + suit + "</li>");
+
+    // Word Order
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    var message = $("#message").val();
+
+    var messagesArr = message.split(" ");
+    var finalMessage = [];
+
+    messagesArr.forEach(function(word1) {
+      var wordCount = 0;
+      messagesArr.forEach(function(word2){
+        if (word1 === word2) {
+          wordCount += 1;
+        }
+      });
+      finalMessage.push(word1, wordCount);
     });
+
+    alert(finalMessage);
+
+
   });
-
-
 });
